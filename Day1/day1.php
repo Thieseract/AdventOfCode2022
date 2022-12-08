0fe53a4985d4dd797182eb882a32e7cc4f456ae1
Tim Lassiter <thieseract@gmail.com>
2022-12-07 19:17:46 -0500

    day1 adjustment

--- Day1/day1.php
+++ Day1/day1.php
@@ -10,6 +10,7 @@ function solvePuzzle($rations){
 
   $caloriesPerElf = [];
   $totalCaloriesTop3 = 0;
+  $mostCalories = 0;
 
   // loop through each elf's inventory, adding up the contents, and compare to current highest count
   foreach($inventoryPerElf as $elf){
@@ -20,6 +21,10 @@ function solvePuzzle($rations){
     $snacks = explode(PHP_EOL, $elf);
     foreach($snacks as $snack){
       $snackTotal += $snack;
+    }
+
+    if($snackTotal > $mostCalories){
+      $mostCalories = $snackTotal;
     }
 
     $caloriesPerElf[] = $snackTotal;
@@ -33,8 +38,11 @@ function solvePuzzle($rations){
     $totalCaloriesTop3 += $caloriesPerElf[$i];
   }
 
+  echo 'The Elf with the most total calories is carrying ' . $mostCalories . PHP_EOL;
+  echo 'The top 3 elves are carrying a total of ' . $totalCaloriesTop3;
+
   return $totalCaloriesTop3;
 }
 
-$solution = solvePuzzle($rations);
-echo $solution;
+solvePuzzle($rations);
+
